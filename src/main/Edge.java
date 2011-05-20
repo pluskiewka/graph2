88a948e1f59ced3d1085938bb533005f260eb7c9
pluskiewka <pluskiewka@gmail.com>
2011-05-20 14:37:11 +0200

    Pierwszy commit.

---
+++ src/main/Edge.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.io.Serializable;
+import org.objectweb.proactive.extensions.annotation.ActiveObject;
+
+@ActiveObject
+public class Edge implements Serializable {
+	private static final long serialVersionUID = -6302028332002939511L;
+	
+	public final Vertex v1, v2;
+	public int level, color;
+	
+	public Edge(Vertex v1, Vertex v2) {
+		this.v1 = v1;
+		this.v2 = v2;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj == this || (obj instanceof Edge &&  ((Edge)obj).v1.equals(this.v1) && ((Edge)obj).v2.equals(this.v2));
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.v1.id * this.v2.id;
+	}
+}
---
+++ src/main/Vertex.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.io.Serializable;
+import org.objectweb.proactive.extensions.annotation.ActiveObject;
+
+@ActiveObject
+public class Vertex implements Serializable {
+	private static final long serialVersionUID = -6300664513616426780L;
+	
+	public final int id;
+	
+	public Vertex(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj == this || (obj instanceof Vertex && ((Vertex)obj).id == this.id);
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.id;
+	}
+}

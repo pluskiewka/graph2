eb2a3d6e6b54b8580f34e46b073494170fe45c47
pluskiewka <pluskiewka@gmail.com>
2011-05-31 22:31:51 +0200

    Dodanie skryptów, opisanie w README.

--- cmd/compile.sh
+++ cmd/compile.sh
@@ -11,3 +11,4 @@ cd ../src
 javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Node.java 
 javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Client.java 
 javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Server.java 
+javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Auto.java 

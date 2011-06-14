91e589c0b488f0e613fba9ee2b68427a81171fad
pluskiewka <pluskiewka@gmail.com>
2011-06-14 08:23:23 +0200

    Update skryptów do uruchomienia aplikacji.

--- cmd/auto.sh
+++ cmd/auto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $# == 1 ] ; then
-	cd ../bin
-	java -Djava.class.path=:../lib/log4j-1.2.16.jar main/Auto $1
+	cd ../bin/main/java
+	java -Djava.class.path=:./lib/log4j-1.2.16.jar main/Auto $1
 else
 	echo "Usage $0 <server>"
 fi
--- cmd/compile.sh
+++ cmd/compile.sh
@@ -3,12 +3,14 @@
 cd ..
 rm -rf bin
 mkdir bin
-cd bin
-ln -s ../lib lib
-ln -s ../cfg cfg
-ln -s ../log log
-cd ../src
-javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Node.java 
-javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Client.java 
-javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Server.java 
-javac -cp :../lib/log4j-1.2.16.jar -d ../bin/ main/Auto.java 
+mkdir bin/main
+mkdir bin/main/java
+cd bin/main/java
+ln -s ../../../lib lib
+ln -s ../../../cfg cfg
+ln -s ../../../log log
+cd ../../../src/main/java
+javac -cp :../../../lib/log4j-1.2.16.jar -d ../../../bin/main/java main/Node.java 
+javac -cp :../../../lib/log4j-1.2.16.jar -d ../../../bin/main/java main/Client.java 
+javac -cp :../../../lib/log4j-1.2.16.jar -d ../../../bin/main/java main/Server.java 
+javac -cp :../../../lib/log4j-1.2.16.jar -d ../../../bin/main/java main/Auto.java 
